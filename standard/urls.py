bf2235512ef0a2b34aa8a4a97e11546f6f4dcdea
wchriestenson <wadechriestenson@gmail.com>
2022-04-21 11:30:15 -0700

    04/21/2022 - WC

--- standard/urls.py
+++ standard/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('logout/', views.logout, name='logout'),
+    path('user.html', views.user_is_true, name='user')
 ]
--- standard/views.py
+++ standard/views.py
@@ -16,3 +16,7 @@ def logout(request):
     client_id = settings.SOCIAL_AUTH_AUTH0_KEY
     return_to = 'http://127.0.0.1:8000'  # this can be current domain
     return redirect(f'https://{domain}/v2/logout?client_id={client_id}&returnTo={return_to}')
+
+
+def user_is_true(request):
+    return render(request, 'user.html', {})
